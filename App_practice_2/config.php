6873cba476a787844a050315c40a706219c38559
趙珣雅 <jhao@zhaoxunyadeMacBook-Pro.local>
2020-04-06 11:18:52 +0800

    remote integration

---
+++ App_practice_2/config.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return $config =['connection' => 'mysql',
+                'name'=>'mytodos',
+                'hostname'=>'127.0.0.1',
+                'username'=>'root',
+                'password'=>'',
+                'options'=>[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]
+];
